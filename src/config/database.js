da76ee9a70837166ae9222325c7f91bf18aee6bb
Thiago Frazão <thiagofrazao@aluno.uema.br>
2025-06-29 11:18:29 -0300

    configura conexão inicial com o banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB Conectado...');
+        console.log('Conexão com o banco feita com sucesso!');
     } catch (err) {
         console.error(err.message);
         process.exit(1); // Sai do processo com falha
